2582a07d03982202bbaf5b032911fe8d5783c8ec
ajaysamp <ajaysamp@gmail.com>
2017-10-26 18:11:38 -0700

    Add files via upload

---
+++ EX_3_Titanic_GGPLOT/Titanic.R
@@ -0,0 +1,9 @@
+str(titanic)
+
+ggplot(titanic, aes(x = Pclass, fill = Sex)) + geom_bar(position = "dodge")
+
+ggplot(titanic, aes(x = Pclass, fill = Sex)) + geom_bar(position = "dodge") + facet_grid(.~Survived)
+
+posn.jd <- position_jitterdodge(0.5, 0, 0.6)
+
+ggplot(titanic, aes(x = Pclass, y = Age, color = Sex)) + geom_point(position = posn.jd, size = 3, alpha = 0.5) + facet_grid(.~Survived)
